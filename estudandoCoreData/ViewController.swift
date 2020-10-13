040e5514734f9f8ce3e79d203b9ceab3baebe06f
Joao Silva <josejoaosilva.n@gmail.com>
2020-10-13 10:31:00 -0300

    first commit

---
+++ estudandoCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  estudandoCoreData
+//
+//  Created by José João Silva Nunes Alves on 13/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
